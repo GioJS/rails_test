ec1663fcc5cd191ad6d11e106bd25e1aa62fea4a
giordano cristini <giordano.cristini@exprivia.com>
2018-11-09 09:31:46 +0100

    dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,6 +13,8 @@ Rails.application.configure do
   config.consider_all_requests_local = false
 
   config.action_controller.perform_caching = true
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.read_encrypted_secrets = true
 
 
   # Don't care if the mailer can't send.
